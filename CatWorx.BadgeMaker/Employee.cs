57ef2a8bc50bfebee312a3c19882361ddb39ffcc
Matthew Garry <mhgarry92@gmail.com>
2023-10-12 17:27:59 -0400

    create a usable class constructor for employee class to use in program

--- CatWorx.BadgeMaker/Employee.cs
+++ CatWorx.BadgeMaker/Employee.cs
@@ -3,5 +3,12 @@ namespace CatWorx.BadgeMaker //namespace our container for our program
     class Employee //this class stores employee info
     {
         public string FirstName; //stores employees first name to be publically used throughout program
+        public string LastName; //stores employees last name to be publically used throughout program
+        public int Id; //assigns an id to each employee
+        public string PhotoUrl; //links a url of a photo to each employee
+        public Employee(string firstName, string lastName) { //our class constructor for our Employee class
+            FirstName = firstName;
+            LastName = lastName;
+        }
     }
 }
