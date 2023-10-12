b8ecc01ecbc5a2288dcbc5634fed42b5a4cf8983
Matthew Garry <mhgarry92@gmail.com>
2023-10-12 17:34:51 -0400

    update our main program to include Employee class as list instead of list of strings

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -5,7 +5,7 @@ namespace CatWorx.BadgeMaker //our entry point to our file. Like a container to 
 {
     class Program
     {
-        static List<string> GetEmployees() //we are creating a method as an empty list to store a list of strings classified as employees
+        static List<Employee> GetEmployees() //we are creating a method as an empty list to store a list of strings classified as employees
         {
             List<string> employees = new List<string>(); //we are creating an empty list of strings to populate with our argument 'employees'
             while (true) //we are creating a while loop that will continue while the following conditions are true
@@ -24,16 +24,16 @@ namespace CatWorx.BadgeMaker //our entry point to our file. Like a container to 
             }
             return employees; //this line returns our newly forumlated list of employees fufilling our agreeent to create a new List with a string of employees
         }
-        static void PrintEmployees(List<string> employees) //we are creating a method to print our list of employees to the conosle. Threfore we are inputting our List generated from our method GetEmployees()
+        static void PrintEmployees(List<Employee> employees) //we are creating a method to print our list of employees to the conosle. Threfore we are inputting our List generated from our method GetEmployees()
         {
             for (int i = 0; i < employees.Count; i++) //iterate through the list of employees
             {
-                Console.WriteLine(employees[i]); //write each indexed employee individually to the console
+                Console.WriteLine(employees[i].GetFullName()); //write each indexed employee individually to the console
             }
         }
         static void Main(string[] args) //our entry point to our program that will consume both of our methods to make a program that takes input from the console and prints it out
         {
-            List<string> employees = GetEmployees(); //Starts our get employees function method when called as an argument and returns our list of employees
+            List<Employee> employees = GetEmployees(); //Starts our get employees function method when called as an argument and returns our list of employees
             PrintEmployees(employees); //recieves our list of employees from our GetEmployees() method and prints them to the console
         }
     }
