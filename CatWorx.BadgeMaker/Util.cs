6373c08fa776fe4e522bf651bc3abe756d6a1a88
Matthew Garry <mhgarry92@gmail.com>
2023-10-12 18:24:30 -0400

    move PrintEmployees method to Utils class

--- CatWorx.BadgeMaker/Util.cs
+++ CatWorx.BadgeMaker/Util.cs
@@ -2,6 +2,14 @@ namespace CatWorx.BadgeMaker //namespace
 {
     class Util //class for utility methods/functions
     {
-        
+        public static void PrintEmployees(List<Employee> employees) //declared method as static which means it belongs to util class 
+        {
+             for (int i = 0; i < employees.Count; i++) //iterate through the list of employees
+            {
+                string employeeTemplate = "{0, -10}\t{1, -20}\t{2}"; //formatting our templated string
+                Console.WriteLine(String.Format(employeeTemplate, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl())); //write each indexed employee and their information individually to the console 
+            }
+        }
+
     }
 }
