4ad2570446b9db5880c84957b413798ba986c1d2
Matthew Garry <mhgarry92@gmail.com>
2023-10-12 17:52:17 -0400

    change values inside Employees class to private to encapsulate them in class

--- CatWorx.BadgeMaker/Employee.cs
+++ CatWorx.BadgeMaker/Employee.cs
@@ -2,10 +2,10 @@ namespace CatWorx.BadgeMaker //namespace our container for our program
 {
     class Employee //this class stores employee info
     {
-        public string FirstName; //stores employees first name to be publically used throughout program
-        public string LastName; //stores employees last name to be publically used throughout program
-        public int Id; //assigns an id to each employee
-        public string PhotoUrl; //links a url of a photo to each employee
+        private string FirstName; //stores employees first name to be publically used throughout program
+        private string LastName; //stores employees last name to be publically used throughout program
+        private int Id; //assigns an id to each employee
+        private string PhotoUrl; //links a url of a photo to each employee
         public Employee(string firstName, string lastName, int id, string photoUrl)
         { //our class constructor for our Employee class
             FirstName = firstName;
