57026396c80b604ef13c1e73f757e95a042f97d9
Matthew Garry <mhgarry92@gmail.com>
2023-10-12 21:06:43 -0400

    add SkiaSharp and code to create .png badges

--- CatWorx.BadgeMaker/Util.cs
+++ CatWorx.BadgeMaker/Util.cs
@@ -1,6 +1,9 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using SkiaSharp;
 
 namespace CatWorx.BadgeMaker //namespace
 {
@@ -20,22 +23,27 @@ namespace CatWorx.BadgeMaker //namespace
             {   //if doesn't exist create directory
                 Directory.CreateDirectory("data");
                 //use StreamWRiter to write a new .csv file of employees
-                using (StreamWriter file = new StreamWriter("data/employees.csv"))
-                {   //write "ID, Nmae, and PhotoUrl" into file
-                    file.WriteLine("ID,Name,PhotoUrl");
+            }
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {   //write "ID, Nmae, and PhotoUrl" into file
+                file.WriteLine("ID,Name,PhotoUrl");
 
                     //loop over employees
-                    for (int i = 0; i < employees.Count; i++)
-                    {
-                        //write each employee to the csv file 
-                        string employeeTemplate = "{0},{1},{2}";
-                        file.WriteLine(String.Format(employeeTemplate, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));                        
-                    }
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    //write each employee to the csv file 
+                    string employeeTemplate = "{0},{1},{2}";
+                    file.WriteLine(String.Format(employeeTemplate, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));                        
                 }
             }
         }
-        //Loop over the given employeees 
-        //Write each employee's info as a comma-seperated string using csv file
-
+        public static void MakeBadges(List<Employee> employees)
+        {
+         //create image 
+        SKImage newImage = SKImage.FromEncodedData(File.OpenRead("badge.png"));
+        // save image to data and encode it to write it to a png file
+        SKData data = newImage.Encode();
+        //create file in data called employeeBadge.png
+        data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
     }
 }
